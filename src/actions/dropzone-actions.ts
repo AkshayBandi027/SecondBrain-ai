141d290c046c0f0addf7a87d654da41f0dd05c5a
akshay027 <akshaybandi271@gmail.com>
2024-10-19 10:17:58 +0530

    set up pinecone & store vectors

--- src/actions/dropzone-actions.ts
+++ src/actions/dropzone-actions.ts
@@ -5,39 +5,37 @@ import { TextLoader } from "langchain/document_loaders/fs/text";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { embeddings } from "@/lib/embeddings";
 import { v4 as uuidv4 } from 'uuid';
+import {index} from "@/lib/pinecone"
+import { convertToAscii } from "@/lib/utils";
 
-type PDFpage = {
-  pageContent: string
-  metadata: {
-    loc: {
-      pageNumber: number
-    }
-  }
-}
 
-export const extractText = async (url: string,fileName: string,fileType: string) => {
- try {
-  let loader;
-  console.log(`fileType---`,fileType)
-
-   if(fileType === "text/plain" || fileType === "text/markdown"){
+const loader = async (url: string,fileType: string) => {
+  try {
+    let loader;
+  
+  if(fileType === "text/plain" || fileType === "text/markdown"){
     const response = await fetch(url)
     if(!response.ok) {
       throw new Error("Failed to fetch file")
     }
     const text = await response.text()
     loader = new TextLoader(new Blob([text]))
- } 
- 
- else if (fileType === "application/pdf" || fileType === "pdf") {
-  const response = await fetch(url)
-  const arrayBuffer = await response.arrayBuffer()
-  console.log(`processing`)
-   loader = new PDFLoader(new Blob([arrayBuffer], { type: fileType }))
- }
+  } else if (fileType === "application/pdf" || fileType === "pdf") {
+    const response = await fetch(url)
+    const arrayBuffer = await response.arrayBuffer()
+    loader = new PDFLoader(new Blob([arrayBuffer], { type: fileType }))
+  }
+  return await loader?.load()
+  } catch (error) {
+    console.log(`error while loading file`,error)
+  }
+}
 
- const docs = await loader?.load()
- console.log(`docs---`,docs)
+
+export const extractText = async (url: string,fileName: string,fileType: string) => {
+ try {
+
+ const docs = await loader(url,fileType)
 
  if(!docs) {
   throw new Error("Failed to load document")
@@ -50,16 +48,15 @@ export const extractText = async (url: string,fileName: string,fileType: string)
 
  const splitDocs = await textSplitter.splitDocuments(docs)
  
- const serializedDocs = splitDocs.map((doc: PDFpage) => ({
+ const processedDocs = splitDocs.map((doc: {pageContent: string,metadata: {loc: {pageNumber: number}}}) => ({
   pageContent: doc.pageContent.replace(/\n/g, " "),
   metadata: {
      pageNumber: doc.metadata.loc.pageNumber
   }
  }))
 
- const vectorEmbeddings =  await Promise.all(serializedDocs.map(async(doc: {pageContent: string, metadata: {pageNumber: number}}) => {
+ const vectorEmbeddings =  await Promise.all(processedDocs.map(async(doc: {pageContent: string, metadata: {pageNumber: number}}) => {
    const embedding =  await embeddings.embedQuery(doc.pageContent)
-   console.log(doc)
    const id = uuidv4()
    return {
       id,
@@ -71,7 +68,8 @@ export const extractText = async (url: string,fileName: string,fileType: string)
  }
 ))
 
-console.log(vectorEmbeddings, `--- vectorEmbeddings`)
+ const namespace = index.namespace(convertToAscii(fileName))
+ await namespace.upsert(vectorEmbeddings)
 
 } catch (error) {
   console.log(error)
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const convertToAscii = (str: string) => {
+  return str.replace(/[^\x00-\x7F]+/g, "")
+}
